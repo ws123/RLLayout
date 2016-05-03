5d5e29dc5d4215b4b512ded188d77ada06be629a
carlos <372532647@qq.com>
2016-05-03 19:22:53 +0800

    增加注释

--- mylibrary/src/main/java/com/carlos/IHeaderView.java
+++ mylibrary/src/main/java/com/carlos/IHeaderView.java
@@ -16,24 +16,24 @@ public interface IHeaderView {
     View initHeaderView();
 
     /**
-     * 下拉方法,表示整个View正在被下拉出现<p/>
-     * 在下拉过程中,这个方法会被多次调用<p/>
-     * 可以根据偏移量的大小,在这里做一些动画,比如偏移量越大,箭头越来越大,颜色越来越重等等<p/>
-     * 如果下拉超过临界后,会调用passThreshold()方法<p/>
+     * 下拉方法,表示整个View正在被下拉出现
+     * 在下拉过程中,这个方法会被多次调用
+     * 可以根据偏移量的大小,在这里做一些动画,比如偏移量越大,箭头越来越大,颜色越来越重等等
+     * 如果下拉超过临界后,会调用passThreshold()方法
      *
-     * @param translateY 下拉的偏移量<p/>
-     *                   在下拉过程中,会有一个临界值,<p/>
-     *                   如果小于临界值,translateY会越来越大<p/>
-     *                   如果大于临界值,即使再继续下拉,translateY也不会再变大<p/>
-     *                   如果一直小于临界值,用户没有松手,而是又往上滑,translateY会慢慢变小,最小为0<p/>
-     *                   如果大于临界值,用户没有松手,又往上滑,直到小于临界值,translateY才会慢慢变小,最小为0<p/>
-     * @param view       headView,就是View initHeaderView()这个方法返回的View实例<p/>
+     * @param translateY 下拉的偏移量
+     *                   在下拉过程中,会有一个临界值,
+     *                   如果小于临界值,translateY会越来越大
+     *                   如果大于临界值,即使再继续下拉,translateY也不会再变大
+     *                   如果一直小于临界值,用户没有松手,而是又往上滑,translateY会慢慢变小,最小为0
+     *                   如果大于临界值,用户没有松手,又往上滑,直到小于临界值,translateY才会慢慢变小,最小为0
+     * @param view       headView,就是View initHeaderView()这个方法返回的View实例
      */
     void pullingDown(View view, int translateY);
 
     /**
-     * 用户下拉超过临界值后,松开手指会调用这个方法<p/>
-     * 如果没有松手,即使超过临界值,也不会调用这个方法<p/>
+     * 用户下拉超过临界值后,松开手指会调用这个方法
+     * 如果没有松手,即使超过临界值,也不会调用这个方法
      *
      * @param threshold 临界值
      * @param view      headView,就是View initHeaderView()这个方法返回的View实例
@@ -49,9 +49,9 @@ public interface IHeaderView {
     void backToThreshold(View view, int threshold);
 
     /**
-     * 下拉超过临界值后,用户松开手机,会调用这个方法,也就是执行下拉刷新<p/>
-     * 也就是说,这个方法一定会在passThreshold()之后调用<p/>
-     * 但调用passThreshold()后,如果用户没有松开手指,这个方法不会调用<p/>
+     * 下拉超过临界值后,用户松开手机,会调用这个方法,也就是执行下拉刷新
+     * 也就是说,这个方法一定会在passThreshold()之后调用
+     * 但调用passThreshold()后,如果用户没有松开手指,这个方法不会调用
      *
      * @param view headView,就是View initHeaderView()这个方法返回的View实例
      */
